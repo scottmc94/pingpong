d1fce407f4a799444cebc5943279cf5b2653281d
Scott McPheeters <scottmc94@gmail.com>
2014-02-14 21:53:02 -0500

    added record to index and updated player page to show current stats

--- trues.py
+++ trues.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 #
 
-
 from trueskill import Rating, quality_1vs1, rate_1vs1
 from collections import defaultdict
 
@@ -14,10 +13,8 @@ fn_rating = open("rating.txt","w")
 players = {}
 report = []
 pm = defaultdict(list)
-
-
-#players['neverplayed'] = Rating()
-
+pwin = defaultdict(int)
+plos = defaultdict(int)
 
 for line in file:
   mysp = line.rstrip('\n').split(",")
@@ -29,10 +26,10 @@ for line in file:
   sc = mysp[5]+"-"+mysp[6] 
   wr =  str("%.2f" % players[mysp[2]].mu)
   lr = str("%.2f" % players[mysp[3]].mu)
+  pwin[mysp[2]] += 1 
+  plos[mysp[3]] += 1 
   pm[mysp[2]].append([int(mysp[0]),mysp[4],mysp[3],oldloser,"W",sc,wr])
   pm[mysp[3]].append([int(mysp[0]),mysp[4],mysp[2],oldwin,"L",sc,lr])
-
-
 
   fn_rating.write('%s,%s,%s,%.2f,%.2f,%s\n' % (mysp[0],mysp[1],mysp[2],players[mysp[2]].mu,players[mysp[2]].sigma, mysp[4]))
   fn_rating.write('%s,%s,%s,%.2f,%.2f,%s\n' % (mysp[0],mysp[1],mysp[3],players[mysp[3]].mu,players[mysp[3]].sigma,mysp[4]))
@@ -55,11 +52,11 @@ ht.write("<body>\n")
 ht.write("<h1>Ratings from Ladder Matches</h1>\n")
 ht.write("as of: %s %s vs %s %s-%s<p>\n" % (mysp[4],mysp[2],mysp[3],mysp[5],mysp[6]))
 ht.write('<table class="bordered">\n')
-ht.write("<thead><tr>\n\t<th>Rank</th>\n\t<th>Name</th>\n\t<th>Rating</th>\n\t<th>Sigma</th>\n</tr></thead>\n")
+ht.write("<thead><tr>\n\t<th>Rank</th>\n\t<th>Name</th>\n\t<th>Record</th>\n\t<th>Rating</th>\n\t<th>Sigma</th>\n</tr></thead>\n")
 for rep in report:
   x += 1
   print x,rep[0], "%.2f" % rep[1], "%.2f" % rep[2]
-  ht.write("<tr>\n\t<td>%d</td>\n\t<td><a href=%s.html>%s</a></td>\n\t<td>%.2f</td>\n\t<td>%.2f</td>\n</tr>" % (x,rep[0],rep[0],rep[1],rep[2]))
+  ht.write("<tr>\n\t<td>%d</td>\n\t<td><a href=%s.html>%s</a></td>\n\t<td>%d-%d</td><td>%.2f</td>\n\t<td>%.2f</td>\n</tr>" % (x,rep[0],rep[0],pwin[rep[0]],plos[rep[0]],rep[1],rep[2]))
 ht.write("</table>\n")
 ht.write("<p>")
 ht.write("Rating value follows player's win/draw/lose records. Higher value means higher game skill. And sigma value follows the number of games. Lower value means many game plays and higher rating confidence </body>\n</html>\n")
@@ -84,7 +81,8 @@ for k in pm:
     ht.write("</head>\n")
     ht.write("<body>\n")
     ht.write("<h1>%s</h1>\n" % (k))
-    ht.write("<a href=index.html>index</a>\n")
+    ht.write("<h2>Record: %d-%d Rating: %.2f Sigma: %.2f</h2>\n" % (pwin[k],plos[k],players[k].mu,players[k].sigma))
+    ht.write('<div align="right"><a href=index.html>Return to Index</a></div>\n')
     ht.write('<table class="bordered">\n')
     ht.write("<thead><tr>\n\t<th>Date</th>\n\t<th>Vs</th>\n\t<th>Result</th>\n\t<th>Score</th>\n\t<th>New Rating</th>\n</tr></thead>\n")
     for j in pm[k]:
@@ -95,7 +93,9 @@ for k in pm:
         ht.write("<td>%s</td>\n\t" % j[5])
         ht.write("<td>%s</td>\n\t" % j[6])
         ht.write("</tr>\n")
-    ht.write("</table>\n</body>\n</html>\n")
+    ht.write("</table>\n")
+    ht.write('<div align="right"><a href=index.html>Return to Index</a></div>\n')
+    ht.write("</body>\n</html>\n")
     ht.close()
     fname = None
     ht = None
